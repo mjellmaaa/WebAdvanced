6b2b6ba1dd43e32b89ec910580f935af0321b64b
mjellmaaa <mjellmacitaku1@gmail.com>
2026-03-30 16:15:57 +0200

    commit

--- lesson26/main.js
+++ lesson26/main.js
@@ -1,6 +1,6 @@
 console.log('gooodbye')
 
-var x = 5;
+//var x = 5;
 
 var a = 50 + 50;
 console.log(a);
@@ -19,3 +19,66 @@ var h = 10;
 var i = 4;
 var j = h % i;
 console.log(j);
+
+var k = 99;
+k++;
+console.log(k);
+
+var l = 113;
+l--;
+console.log(l);
+    
+var p = 20;
+var q = p**2;
+console.log(q);
+
+var x = 15;
+x+=5;
+console.log(x);
+
+var m = 25;
+m-=5;
+console.log(m);
+
+var o = 30;
+o/=10;
+console.log(o);
+
+var z = 60;
+z*=2;
+console.log(z);
+
+var r = 10;
+var u = 10;
+console.log(r == u);
+
+var t = 6;
+var s = 5;
+console.log(t != s);
+
+var num1 = 10;
+var num2 = 1;
+console.log(num1 > num2);
+
+var num3 = 2;
+var num4 = 1;
+console.log(num3 < num4);
+
+var num5 = 123;
+var num6 = "123";
+console.log(num5 === num6);
+
+var firstname = "digital";
+var lastname = "school";
+var fullname = firstname + " " + lastname;
+console.log(fullname);
+
+var school =  "digital school";
+console.log(`Hello from ${school}`);
+
+var age = 23;
+console.log(age >= 15 && age <= 67);
+
+var var1 = 5;
+var var2 = 6;
+console.log(var1 == 5 || var2 == 5);
---
+++ lesson27/main.js
@@ -0,0 +1,10 @@
+function showMessage(){
+    alert("this message is inside the function");
+}
+
+showMessage();
+
+function sum(number1 , number2){
+    return number1 + number2;
+}
+console.log(sum(25,5));
