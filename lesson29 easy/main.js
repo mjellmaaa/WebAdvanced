a1c72cec7921212e6f4d0a29bac8b5c59fcab835
mjellmaaa <mjellmacitaku1@gmail.com>
2026-04-22 16:06:29 +0200

    commit me zor ekom bo arsimtare

---
+++ lesson29 easy/main.js
@@ -0,0 +1,8 @@
+var students = ['mjellma', 'rita', 'festina', 'anisa', 'valza', 'diella'];
+console.log(students);
+
+var books = ['p4', 'nuk di'];
+
+var [s1,s2] = students;
+console.log(s1);
+console.log(s2);
