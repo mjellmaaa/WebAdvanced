522908aac9faeacc2b7230fd850a0d42bd8c481f
mjellmaaa <mjellmacitaku1@gmail.com>
2026-05-04 16:04:53 +0200

    commit

---
+++ lesson31/main.js
@@ -0,0 +1,67 @@
+//do while loop
+
+var i = 0;
+do{
+    i  = i + 1;
+    console.log(i);
+}while(i < 5);
+console.log("-----------");
+
+
+//while loop
+
+var j = 0;
+while(j < 5){
+    j = j + 1;
+    console.log(j);
+}
+console.log("------------");
+
+let n = 0;
+let x = 0;
+while(n < 3){
+    n++;
+    x += n;
+    console.log(n);
+    console.log(x);
+}
+console.log("------------");
+
+// for loop
+
+for(var i = 0; i < 5; i++){
+    console.log("value of i is: " + i);
+}
+
+//for/in
+
+var person = {firstname: "Rita", lastName: "Bekteshi", age: 13};
+
+var text = '';
+
+var z;
+
+for (z in person){
+    text += person[z];
+}
+console.log(text);
+
+console.log("------------")
+
+//for/of
+var names = ['reis', 'nehar', 'bardh'];
+
+var  y;
+
+for (y of names){
+    console.log(y);
+}
+console.log("-------------")
+
+var txt = "Javascript";
+
+var l;
+
+for(l of txt){
+    console.log(1);
+}
