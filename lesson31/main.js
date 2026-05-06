8b76916c21b0c5a547abc3f3bb3e3bc711302878
mjellmaaa <mjellmacitaku1@gmail.com>
2026-05-06 16:11:01 +0200

    commit

--- lesson31/main.js
+++ lesson31/main.js
@@ -1,67 +1,57 @@
-//do while loop
+//regEx
 
-var i = 0;
-do{
-    i  = i + 1;
-    console.log(i);
-}while(i < 5);
-console.log("-----------");
+var input = document.getElementById('input_id');
+var button = document.getElementById('btn_id');
+var text = document.getElementById('teksti_id');
 
-
-//while loop
-
-var j = 0;
-while(j < 5){
-    j = j + 1;
-    console.log(j);
-}
-console.log("------------");
-
-let n = 0;
-let x = 0;
-while(n < 3){
-    n++;
-    x += n;
-    console.log(n);
-    console.log(x);
-}
-console.log("------------");
-
-// for loop
-
-for(var i = 0; i < 5; i++){
-    console.log("value of i is: " + i);
+button.onclick = function(){
+    text.innerHTML = input.value;
 }
 
-//for/in
+// var text = "the best school in the world is Digital School";
+// var result = text.search("Digital School");
+// document.getElementById("result1").innerHTML = result;
 
-var person = {firstname: "Rita", lastName: "Bekteshi", age: 13};
+// var text = "the best school in the world is Digital School";
+// var result = text.search(/Digital School/);
+// document.getElementById("result2").innerHTML = result;
 
-var text = '';
+// var text = "the best school in the world is Digital School";
+// var result = text.replace(/Digital School/, "another school");
+// document.getElementById("result3").innerHTML = result;
 
-var z;
+// var text = "abcdef";
+// var regex = new RegExp('abc');
+// document.getElementById('result4').innerHTML = regex.test(text);
 
-for (z in person){
-    text += person[z];
-}
-console.log(text);
+// var text = "my school is the best school in the world";
+// var regex = /school/g;
+// document.getElementById("result5").innerHTML = text.match(regex);
 
-console.log("------------")
+// var text = "my school is the best school in the world";
+// var regex = /o/g;
+// document.getElementById("result6").innerHTML = text.match(regex);
 
-//for/of
-var names = ['reis', 'nehar', 'bardh'];
+// var text = "my school is the best school in the world";
+// var regex = /[abc]/g;
+// document.getElementById("result7").innerHTML = text.match(regex);
 
-var  y;
+// var text = "digital school is in top 10 best school of the world";
+// var regex = /[0-9]/g;
+// document.getElementById("result8").innerHTML = text.match(regex);
 
-for (y of names){
-    console.log(y);
-}
-console.log("-------------")
+// var text = "my school is the best school in the world";
+// var regex = /top|best|school/g;
+// document.getElementById("result9").innerHTML = text.match(regex);
 
-var txt = "Javascript";
+// var text = "100 percent";
+// var regex = /\d/g;
+// document.getElementById("result10").innerHTML = text.match(regex);
 
-var l;
+// var text = "my school is the best school in the world";
+// var regex = /\s/g;
+// document.getElementById("result11").innerHTML = text.match(regex);
 
-for(l of txt){
-    console.log(1);
-}
+var text = "heeey, how are you";
+var regex = /e+/g;
+document.getElementById("result12").innerHTML = text.match(regex);
